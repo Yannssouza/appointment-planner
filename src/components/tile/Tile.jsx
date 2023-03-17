6507c6515d193f1f186ef7a315559855e07b455b
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-03-17 12:37:26 -0300

    commit2

--- src/components/tile/Tile.jsx
+++ src/components/tile/Tile.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
-  return <div className="tile-container"></div>;
+export const Tile = ({ tile }) => {
+  return (
+    <div className="tile-container">
+      {Object.values(tile).map((value, index) => (
+        <p key={index} className={`${index === 0 ? "tile-title" : ""} tile`}>
+          {value}
+        </p>
+      ))}
+    </div>
+  );
 };
